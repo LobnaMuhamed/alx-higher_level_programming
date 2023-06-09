28f9905d3b908dbe23adaa265cdbed5de003becd
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-06-08 19:06:54 -0700

    solve task no 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    len_arg = len(sys.argv) - 1
+    if len_arg == 0:
+        print("{} argument.")
+    elif len_arg == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len_arg))
+    for i in range(len_arg):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
