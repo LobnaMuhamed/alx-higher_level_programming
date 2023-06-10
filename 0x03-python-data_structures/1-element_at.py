e942380112d48ab3f22ad36ac6d04d327539b544
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-06-10 10:11:09 -0700

    solve task no 2

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """
+    Retrieves an element from a list like in C
+    ...
+
+    Parameters
+    ----------
+    my_list : list optional
+        The list of integers
+    idx: int
+        The given index
+    Return:
+        if idx is negative, return None
+        if idx is out of range, return None
+        else => return element when found
+    """
+    len_list = len(my_list)
+
+    if idx < 0 or idx > len_list:
+        return None
+    for i in range(len_list):
+        if i == idx:
+            return (my_list[i])
