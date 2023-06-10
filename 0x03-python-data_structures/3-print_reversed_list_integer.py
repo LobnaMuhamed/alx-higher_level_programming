6114df37ee8c4e394c0ab4e536f38606391364e4
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-06-10 11:43:49 -0700

    solve task 4

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    """
+    a function that prints all integers of a list, in reverse order.
+    ....
+    Parameters:
+    my_list: list => The list of integers
+    Return:
+       return New reverse List
+    """
+    len_list = len(my_list) - 1
+    for i in range(len_list, -1, -1):
+        print(my_list[i])
