50d415b44f82b188955e04d98122c027f12d6518
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-06-10 11:56:43 -0700

    solve task 5

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """
+    replaces an element in a list at a specific position without modifying the original list
+    .....
+    Parameters: 
+    my_list: list
+        The list of elements
+    idx: int
+        The given index
+    element: the given element
+    """
+    len_list = len(my_list) - 1
+    new_list = []
+    new_list = my_list.copy()
+    if idx < 0 or idx > len_list:
+        return my_list
+    else:
+        new_list[idx] = element
+    return new_list
