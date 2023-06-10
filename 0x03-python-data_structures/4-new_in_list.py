0a87b2b0bb73d61d399b00009d7eb93c11070c23
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-06-10 12:00:46 -0700

    fix task 5

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     """
-    replaces an element in a list at a specific position without modifying the original list
+    replaces an element in a list without modifying the original list
     .....
-    Parameters: 
+    Parameters:
     my_list: list
         The list of elements
     idx: int
@@ -11,8 +11,8 @@ def new_in_list(my_list, idx, element):
     element: the given element
     """
     len_list = len(my_list) - 1
-    new_list = []
     new_list = my_list.copy()
+
     if idx < 0 or idx > len_list:
         return my_list
     else:
