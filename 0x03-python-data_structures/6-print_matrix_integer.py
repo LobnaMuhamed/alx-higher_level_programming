31dc557b148fb38ce3d32bd377e1c097e23e7b7f
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-06-11 06:26:47 -0700

    solve task no 7

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """
+    a function that prints a matrix of integers
+    ......
+    parameters
+    ==========
+    matrix: a input matrix of integers
+    """
+
+    for row in matrix:
+        for i in range(len(row)):
+            if i == len(row) - 1:
+                print("{}".format(row[i]))
+            else:
+                print("{}".format(row[i]), end = " ")
