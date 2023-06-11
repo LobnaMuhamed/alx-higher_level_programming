206b0f1b6aad99bd10ba674b5c83f4ee0518b476
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-06-11 06:47:28 -0700

    test

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,12 +7,9 @@ def print_matrix_integer(matrix=[[]]):
     ==========
     matrix: a input matrix of integers
     """
-    if (len(matrix) - 1) != 0:
-        for row in matrix:
-            for i in range(len(row)):
-                if i == len(row) - 1:
-                    print("{:d}".format(row[i]))
-                else:
-                    print("{:d}".format(row[i]), end=" ")
-    else:
-        print("")
+    for row in matrix:
+        for i in range(len(row)):
+            if i == len(row) - 1:
+                print("{:d}".format(row[i]))
+            else:
+                print("{:d}".format(row[i]), end=" ")
