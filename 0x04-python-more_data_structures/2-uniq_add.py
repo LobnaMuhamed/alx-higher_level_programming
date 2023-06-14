7220a3e7b8a44b5dda1f53533056a9a1b9e9dfa7
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-06-14 16:25:15 -0700

    solve task no 3

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    cp_list = my_list.copy()
-    new_list = []
+    new_list = set(my_list)
     sum = 0
-    for i in cp_list:
-        count = 0
-        for j in my_list:
-            if j == i:
-                count += 1
-            if count == 2:
-                continue
-        if count == 1:
-            new_list.append(j)
-            sum += j
+    for i in new_list:
+        sum += i
     return (sum)
