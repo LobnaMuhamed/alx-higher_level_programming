eb78159b1b29754b611a5193401fdf88e30150e0
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-06-27 19:08:40 -0700

    solve task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+class Square that defines a square
+"""
+
+
+class Square:
+    """
+    class Square that defines a square
+    """
+    def _init_(self, size):
+        """
+        initialize square
+        Parmeters:
+            size: sqaure's size
+        """
+        self.size = size
