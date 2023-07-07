49fe416bbb6bf1a57d0a44fa6ccf86d436465fc4
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-07-06 18:19:17 -0700

    solve task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+""" prevent user from dynamically creating new instaince atrr """
+
+
 class LockedClass:
+    """ prevent user from dynamically creating new instaince atrr """
     __slots__ = ['first_name']
