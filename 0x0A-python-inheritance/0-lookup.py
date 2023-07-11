e46ffc2e3620fc800ec8b9c3a9b3fc3c26b34be5
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-07-10 18:57:34 -0700

    solve task no 1

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" a function that returns the list of available attributes """
+
+
+def lookup(obj):
+    """ a function that returns the list of available attributes """
+
+    return dir(obj)
