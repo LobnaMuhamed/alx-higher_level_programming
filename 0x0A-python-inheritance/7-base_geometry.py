a94c1146b955a071370a4b9ff17759cb0d3610dd
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-07-10 19:31:47 -0700

    solve task no 8

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""" public instance method """
+
+
+class BaseGeometry:
+
+    """ public instance methond """
+    def area(self):
+        """ raise exception """
+        raise Exception("area() is not implemented")
+
+    """ public instance method that validate value """
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
