72c3b80d67e14ae6eaad184a761acdf294f6d0bf
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-07-10 19:26:51 -0700

    solve task no 7

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""" public instance method """
+
+
+class BaseGeometry:
+
+    """ public instance methond """
+    def area(self):
+        """ raise exception """
+        raise Exception("area() is not implemented")
