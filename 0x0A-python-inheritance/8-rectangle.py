2112fa1ad3255dfcefa4873013aeff1b7f4c5a8d
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-07-10 19:41:25 -0700

    solvr task no 9

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" a class Rectangle that inherits from BaseGeometry """
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Class Rectangle """
+
+    def __init__(self, width, height):
+        """ initialize rectangle """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
