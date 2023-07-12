5fdc213876b2d4c6096f8dffc97f6cacafe32809
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-07-11 19:11:24 -0700

    solve task no 7

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Write a function that creates an Object from a “JSON file”
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    {"is_active": true, 12 }
+
+    Parameters:
+        filename: file name
+    """
+    with open(filename, encoding="utf-8") as f:
+        my_json = f.read()
+        return (json.loads(my_json))
