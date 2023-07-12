fe4132ab50f8902ecd8299302b23f528168c31e9
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-07-11 17:59:23 -0700

    fix task no 1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -12,5 +12,5 @@ def read_file(filename=""):
     """
 
     with open(filename, encoding="utf-8") as f:
-        read_file = f.read()
-    print(read_file)
+        for i in f:
+            print(f.read(), end="")
