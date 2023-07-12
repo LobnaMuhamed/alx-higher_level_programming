59212cc049d1a4eaff7b88fea8bfd620a28627b2
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-07-11 17:54:24 -0700

    solve task no 1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,7 +5,11 @@
 
 def read_file(filename=""):
 
-    """ a function that reads a text file (UTF8) """
+    """ a function that reads a text file (UTF8)
+
+        Args:
+            filename (str): the file name
+    """
 
     with open(filename, encoding="utf-8") as f:
         read_file = f.read()
