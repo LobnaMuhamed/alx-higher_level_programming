4af4210af59089536e5f353be0c079fdade959d3
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-07-11 18:25:04 -0700

    fix tasl

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
+"""a function that returns the JSON representation of an object (string) """
 import json
 
-"""a function that returns the JSON representation of an object (string) """
 
 
 def to_json_string(my_obj):
