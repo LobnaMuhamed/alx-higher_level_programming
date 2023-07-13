8825432b5a59f12b1241958904a36f23ec2c8d03
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-07-13 13:27:13 -0700

    solve task no 12

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+a class Student that defines a student
+"""
+
+
+class Student:
+    """
+    intialize class
+
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        returns a dictionary representation
+
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
+
+    def reload_from_json(self, json):
+        """
+        that replaces all attributes of the Student instance
+        """
+        self.first_name = json['first_name']
+        self.last_name = json['last_name']
+        self.age = json['age']
