a36eea5cbc8f60fe67182911fe02f1b75710bf0f
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-07-13 04:41:28 -0700

    solve task no 11

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
- a class Student that defines a student
+a class Student that defines a student
 """
 
 
@@ -10,11 +10,22 @@ class Student:
     intialize class
 
     """
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """ returns a dictionary representaion """
-        return self.__dict__
+    def to_json(self, attrs=None):
+        """
+        returns a dictionary representation
+
+        """
+        if not attrs:
+            return self.__dict__
+        else:
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
