e5fd26bf74c6be4be8c91c5d11a84b3524e0d8d7
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-07-18 03:11:50 -0700

    fix task

--- 0x07-python-test_driven_development/100-main.py
+++ 0x07-python-test_driven_development/100-main.py
@@ -4,4 +4,5 @@ matrix_mul = __import__('100-matrix_mul').matrix_mul
 print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
 print(matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
 
-print(matrix_mul([[1, 2, 9], [3, 4, 2], [7, 8]], [[1, 2], [3, 4]]))
+print (matrix_mul())
+
