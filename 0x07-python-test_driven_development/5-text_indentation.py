bb9e829c9ee974f36ec58507cfa772bfb9e6b301
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-07-17 22:45:48 -0700

    solve task no 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,9 +5,21 @@ a function that prints a text with 2 new lines after each
 of these characters: ., ?, :
 """
 
+
 def text_indentation(text):
     """
     a function that prints a text with 2 new lines after each
     of these characters: ., ?, :
     """
-    
+    if (not isinstance(text, str)):
+        raise TypeError("text must be a string")
+    beginning_of_line = True
+    for i in text:
+        if (i == "." or i == "?" or i == ":"):
+            print("\n")
+            beginning_of_line = True
+        elif i == " " and beginning_of_line:
+            pass
+        else:
+            print(i, end="")
+            beginning_of_line = False
