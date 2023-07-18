329a766e035fe1231bd280fd839b2d11e0ecffef
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-07-17 19:17:49 -0700

    solve task

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,6 +9,5 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     elif ((not isinstance(b, int)) and (not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    
     result = int(a) + int(b)
     return int(result)
