08055489f87a33d58cbeeae049a6190f2c85d245
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-07-17 22:07:35 -0700

    fix task no 3

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+a function that prints a text with 2 new lines after each
+of these characters: ., ?, :
+"""
+
+def text_indentation(text):
+    """
+    a function that prints a text with 2 new lines after each
+    of these characters: ., ?, :
+    """
+    
