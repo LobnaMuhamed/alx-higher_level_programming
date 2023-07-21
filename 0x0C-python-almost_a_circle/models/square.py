7e3e88c36ebe001be315a2854fe29337b4599783
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-07-20 21:33:28 -0700

    fix task

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,4 +19,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """ create __str__ method """
-        return(f"[Square] {self.id} {self.x}/{self.y} - {self.size}")
+        return(f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}")
