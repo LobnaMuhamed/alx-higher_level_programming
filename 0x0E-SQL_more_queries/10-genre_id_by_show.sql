fb7238860912c514e02b7c09a8775eb2b6a80c50
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-08-16 19:16:05 -0700

    solve task no 11

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- a script that lists all shows contained in hbtn_0d_tvshows
 SELECT t.title, tg.genre_id
 FROM tv.shows INNER JOIN tv_show_genres
-ORDER BY tv_shows.title AND tv_show_genres.genre_id; 
+ORDER BY tv_shows.title = tv_show_genres.genre_id; 
