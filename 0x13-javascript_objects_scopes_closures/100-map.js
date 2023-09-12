0e877110a59f5768a342692999726aa35892ba9b
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-09-12 11:18:14 -0700

    fix task no 11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 
 const list = require('./100-data.js').list;
-const newList = [];
-for (let i = 0; i < list.length; i++) {
-  newList.push(list[i] * i);
-}
 console.log(list);
-console.log(newList);
+console.log(list.map((item, idx) => item * idx));
