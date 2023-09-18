3b556ec8e9e364c937eacbcf6a50ff89de130308
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-09-18 14:18:56 -0700

    solve task no 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+a script that lists all states from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    cr = db.cursor()
+    cr.execute("""SELECT * FROM states WHERE name LIKE BINARY '{}'
+                  ORDER BY states.id ASC""".format(sys.argv[4]))
+
+    rows = cr.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cr.close()
+    db.close()
