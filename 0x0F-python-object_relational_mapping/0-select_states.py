84c548c82aea005ae9721f724759c49127721286
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-09-18 13:47:38 -0700

    solve task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -23,15 +23,3 @@ if __name__ == '__main__':
 
     cr.close()
     db.close()
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         password=sys.argv[2], database=sys.argv[3])
-
-    c = db.cursor()
-    c.execute(f"SELECT * FROM states ORDER BY states.id ASC")
-    all_data = c.fetchall()
-
-    for data in all_data:
-        print(data)
-
-    c.close()
-    db.close()
