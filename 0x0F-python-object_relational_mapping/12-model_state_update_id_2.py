a156f08575438f2076a161dc0f677ebfe60d7f68
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-09-19 01:09:19 -0700

    solve task no 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+a script that lists all State objects
+
+from the database hbtn_0e_6_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from model_state import Base, State
+
+if __name__ == '__main__':
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    i = session.query(State).filter(State.id == 2).first()
+    i.name = "New Mexico"
+    session.add(i)
+    session.commit()
+    session.close()
