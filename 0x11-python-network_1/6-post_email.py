f918f4bb8f33120ec6dfa957c22a25a3b4631fd1
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-09-29 19:44:02 -0700

    solve task no 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email
+"""
+
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
