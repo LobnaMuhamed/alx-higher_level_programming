18daa1e5def03720cc94005cbc279cf87f0a6df3
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-09-29 18:18:07 -0700

    solve task no 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL,
+sends a request to the URL
+and displays the value of the X-Request-Id
+"""
+
+
+if __name__ == '__main__':
+    import urllib.request
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = email.encode('utf-8')
+    request = urllib.request.Request(url, data=data, method='POST')
+
+    with urllib.request.urlopen(request) as response:
+        body = response.read().decode('utf-8')
+        print("Your email is: {}".format(body))
