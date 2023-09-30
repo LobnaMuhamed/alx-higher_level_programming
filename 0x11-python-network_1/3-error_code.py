9b8b1d6a38ceaca2c5a1e6e2edf6c64ac48a1c66
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-09-29 19:21:06 -0700

    solve task no 4

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-script that takes in a URL, sends a request to the URL
-and displays the body of the response
+script that takes in a URL and an email,
+sends a POST request to the passed URL
+with the email as a parameter
 """
+import sys
+from urllib import request, error
 
 
 if __name__ == "__main__":
-    import sys
-    from urllib import request, error
-
     try:
         with request.urlopen(sys.argv[1]) as res:
             print(res.read().decode('UTF-8'))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 """
-script that takes in a URL and an email,
-sends a POST request to the passed URL
-with the email as a parameter
+script that fetches https://alx-intranet.hbtn.io/status
 """
-import sys
-from urllib import request, error
+from urllib import request
 
 
 if __name__ == "__main__":
-    try:
-        with request.urlopen(sys.argv[1]) as res:
-            print(res.read().decode('UTF-8'))
-    except error.HTTPError as er:
-        print('Error code:', er.code)
+    response = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
